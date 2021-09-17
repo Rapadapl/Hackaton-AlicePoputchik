65ecb89470774726f92f2314abc98f3ac6278681
GladkikhD <shetup228339@gmail.com>
2021-09-18 09:27:02 +1000

    aaa

--- src/main/kotlin/com/justai/jaicf/template/scenario/HAC7.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/HAC7.kt
@@ -1,3 +1,36 @@
 package com.justai.jaicf.template.scenario
 
-/**/
+import com.justai.jaicf.activator.event.event
+import com.justai.jaicf.builder.Scenario
+import com.justai.jaicf.channel.yandexalice.activator.alice
+import com.justai.jaicf.channel.yandexalice.model.AliceEvent
+import com.justai.jaicf.channel.yandexalice.alice
+
+val HAC7 = Scenario {
+
+    state("main") {
+        activators {
+            event(AliceEvent.START)
+        }
+        action {
+            reactions.say("!")
+        }
+    }
+
+    state("weather") {
+        activators {
+            intent("WEATHER")
+        }
+        action {
+            activator.alice?.run {
+                val where = slots["where"]
+                reactions.say("Определяю погоду в $where")
+                reactions.alice?.endSession()
+            }
+        }
+    }
+
+    fallback {
+        reactions.say("Не понял(-а)")
+    }
+}
